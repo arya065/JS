daa00a701d80c0562c74ece6fcb6637af216aab6
Alexey <arya065@g.educaand.es>
2023-11-15 18:12:38 +0100

    test

--- Scripts/script7.sh
+++ Scripts/script7.sh
@@ -6,5 +6,5 @@ rm file1 file2 file3
 echo "Result saved to $output_file"
 # I have a tar with three files. 
 # You are asked to create a script that generates a toatoa.gz 
-# file with the lines from the first three files that contain the string “ALBORAN”s
+# file with the lines from the first three files that contain the string “ALBORAN”s 
 
