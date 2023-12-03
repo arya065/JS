f3f5c0e3308dcd3225e01b87a1020117180c2ec1
Alexey <arya065@g.educaand.es>
2023-12-03 17:43:59 +0100

    del

--- react/tictactoe/index.js
+++
@@ -1,7 +0,0 @@
-const rootNode = document.getElementById("app"); // элемент для рендеринга приложения React
-// получаем корневой элемент 
-const root = ReactDOM.createRoot(rootNode);
-// рендеринг в корневой элемент
-root.render( /*#__PURE__*/React.createElement("h1", null, "Hello React") // элемент html, который мы хотим создать
-);
-//элементы нельзя изменить, их можно только обновить
