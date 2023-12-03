ec766c4bab07eeddb22814d75daf453bb9cd2ef5
Alexey <arya065@g.educaand.es>
2023-12-03 16:51:20 +0100

    ini

--- react/tictactoe/index.jsx
+++ react/tictactoe/index.jsx
@@ -1,8 +1,8 @@
-// элемент для рендеринга приложения React
-const rootNode = document.getElementById("app");
+const rootNode = document.getElementById("app");    // элемент для рендеринга приложения React
 // получаем корневой элемент 
 const root = ReactDOM.createRoot(rootNode);
 // рендеринг в корневой элемент
 root.render(
-    <h1>Hello React</h1>  // элемент, который мы хотим создать
+    <h1>Hello React</h1>,  // элемент html, который мы хотим создать
 );
+//элементы нельзя изменить, их можно только обновить
