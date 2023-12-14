9942769df0d6444f13d27348504c76acf9c76941
Alexey <arya065@g.educaand.es>
2023-12-14 20:22:46 +0100

    add moving pos

--- react/minevanic/src/Table.js
+++ react/minevanic/src/Table.js
@@ -4,9 +4,18 @@ class Table extends React.Component {
     constructor(props) {
         super(props);
         // здесь добавили стили и сгенерированный массив
-        this.state = { on: "red", off: "green", array: this.fillArr() };
+        this.state = {
+            on: "red",
+            off: "green",
+            array: this.fillArr(),
+            current: [0, 0]
+        };
         this.fillArr = this.fillArr.bind(this);
         this.press = this.press.bind(this);
+        this.incrementX = this.incrementX.bind(this);
+        this.decrementX = this.decrementX.bind(this);
+        this.incrementY = this.incrementY.bind(this);
+        this.decrementY = this.decrementY.bind(this);
     }
     // создание массива единиц
     fillArr() {
@@ -17,7 +26,6 @@ class Table extends React.Component {
                 array[i][j] = 1;
             }
         }
-        // console.log(array);
         return array;
     }
 
@@ -30,15 +38,62 @@ class Table extends React.Component {
         console.log(event);
     }
     // два метода для увеличения и уменьшения счётчика
-    increment() {
-
+    incrementX() {
+        let newArr = this.state.current;
+        if (newArr[0] == 9 && newArr[1] == 9) {
+            newArr[0] = 0;
+            newArr[1] = 0;
+        } else {
+            if (newArr[1] >= 9) {
+                newArr[0] += 1;
+                newArr[1] = 0;
+            } else {
+                newArr[1] += 1;
+            }
+        }
+        this.setState({ current: newArr });
     }
-    decrement() {
-
+    // вывод в консоли при изменении
+    // это если мы создаём новую ссылку на массив, т.к. state обновляется как обезьяна
+    // componentDidUpdate() {
+    //     console.log(this.state.current);
+    // }
+    decrementX() {
+        let newArr = this.state.current;
+        if (newArr[0] == 0 && newArr[1] == 0) {
+            newArr[0] = 9;
+            newArr[1] = 9;
+        } else {
+            if (newArr[1] <= 0) {
+                newArr[0] -= 1;
+                newArr[1] = 9;
+            } else {
+                newArr[1] -= 1;
+            }
+        }
+        this.setState({ current: newArr });
+    }
+    incrementY() {
+        let newArr = this.state.current;
+        if (newArr[0] == 9) {
+            newArr[0] = 0;
+        } else {
+            newArr[0] += 1;
+        }
+        this.setState({ current: newArr });
+    }
+    decrementY() {
+        let newArr = this.state.current;
+        if (newArr[0] == 0) {
+            newArr[0] = 9;
+        } else {
+            newArr[0] -= 1;
+        }
+        this.setState({ current: newArr });
     }
     render() {
-        let arr = this.fillArr();
-        console.log(this.state);
+        // let arr = this.fillArr();
+        // console.log(this.state);
         // this.printArray(this.fillArrState(arr));
         // console.log("react - говна кусок ");
         // this.printArray();
@@ -51,23 +106,25 @@ class Table extends React.Component {
                 <button onClick={this.press} style={{ backgroundColor: this.state.off }}>5</button>
                 <br />
                 {/* это перебор массива */}
-                <table>
+                <table border="1px">
                     <tbody>
-                        {this.state.array.map((el, index) => (
-                            // <br>
-                            <React.Fragment>
-                                <tr>
-                                    <button key={index}>{el}</button>
-                                </tr>
-                                <br />
-                            </React.Fragment>
-
-                        ))};
+                        {this.state.array.map((el, row) => (
+                            <tr key={row}>
+                                {el.map((e, col) => (
+                                    <td>
+                                        <button key={col} className={((this.state.current[0] == row) && (this.state.current[1] == col)) ? "on" : "off"}>{e}</button>
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
                 <br />
-                <button onClick={this.decrement}>{"<<"}</button>
-                <button onClick={this.increment}>{">>"}</button>
+                <button onClick={this.decrementX}>{"<<"}</button>
+                <button onClick={this.incrementX}>{">>"}</button>
+                <br></br>
+                <button onClick={this.decrementY}>{"↑"}</button>
+                <button onClick={this.incrementY}>{"↓"}</button>
             </div >
         );
     }
