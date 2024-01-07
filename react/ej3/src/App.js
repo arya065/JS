da3d598d921a55fde8b212618a404516ccc40674
arya065 <arya065@g.educaand.es>
2024-01-07 18:28:10 +0100

    done?

--- react/ej3/src/App.js
+++ react/ej3/src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deseos: ["gambas", "jamon"]
+    };
+    this.onAdd = this.onAdd.bind(this);
+  }
+  onAdd(event) {
+    event.preventDefault();
+    console.log(event.target.childNodes[0].value);
+    let newList = [];
+    // event.target.childNodes.map(e => {
+    newList.push(event.target.childNodes[0].value);
+    // });
+    this.setState({
+      deseos: [...this.state.deseos, newList]
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <h1>Lista de deseos</h1>
+          <h2>Anade tu regalo favorito</h2>
+          <p>{
+            this.state.deseos.map((e) => (
+              <span>{e};</span>
+            ))
+          }</p>
+          <form onSubmit={this.onAdd}>
+            <input type="text" name="regalo" id="regalo" placeholder="Escribe tu deseo" />
+          </form>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
