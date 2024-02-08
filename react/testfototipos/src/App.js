ce76c16ac90788bba25d5939304af8dac30c9874
arya065 <arya065@g.educaand.es>
2024-02-08 16:19:38 +0100

    ini

--- react/testfototipos/src/App.js
+++ react/testfototipos/src/App.js
@@ -1,25 +1,152 @@
-import logo from './logo.svg';
+import React, { Component, useState } from "react";
+import { List, Button } from 'reactstrap';
+import data from './data/data.json';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+function ShowAnswers(props) {
+  const [answers, setAll] = useState([]);
+  const view = () => {
+    console.log(props.open);
+    if (props.open) {
+      console.log("here");
+      apiGet();
+      console.log(answers);
+    }
+
+  }
+  const apiGet = () => {
+    axios
+      .get("http://localhost/Proyectos/API/apiTestfototipos/add/100/100")
+      .then((response) => {
+        setAll(response.data);
+      })
+      .catch((error) => {
+        console.log("ERRORRRRRRR", error);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {view()}
+    </>
   );
+}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listQuestions: this.getListQuestions(),
+      answers: [],
+      puntuacion: 0,
+      lookAll: false,
+      allAnswers: [],
+    };
+  }
+  getListQuestions() {
+    return data.questions;
+  }
+  addAnswer(i, value) {
+    const tmp = this.state.answers;
+    let prev = 0;
+    if (tmp[i] != undefined) {
+      prev = tmp[i].points;
+    }
+    tmp[i] = value;
+    this.setState({ answers: tmp });
+    this.setState({ puntuacion: this.state.puntuacion + value.points - prev });
+  }
+  showRes() {
+    if (this.state.answers.includes() || this.state.answers.length < 7) {
+      return (
+        <div>
+          <p>PUNTUACION TOTAL....</p>
+          <p>SU TIPO DE PIEL....</p>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <p>PUNTUACION TOTAL - {this.state.puntuacion}</p>
+          <p>SU TIPO DE PIEL{this.getTipoPiel()}</p>
+          <img src={this.getPhoto()} />
+        </div>
+      )
+    }
+  }
+  getTipoPiel() {
+    let points = this.state.puntuacion;
+    if (points <= 7) {
+      return "TIPO DE PIEL I. Muy sensible a la luz solar";
+    } else if (points <= 21) {
+      return "TIPO DE PIEL II. Sensible a la luz solar";
+    } else if (points <= 42) {
+      return "TIPO DE PIEL III. Sensibilidad normal a la luz solar";
+    } else if (points <= 68) {
+      return "TIPO DE PIEL IV. La piel tiene tolerancia a la luz solar";
+    } else if (points <= 84) {
+      return "TIPO DE PIEL V. La piel es oscura. Alta tolerancia";
+    } else {
+      return "TIPO DE PIEL VI. La piel es negra. Altísima tolerancia NIGNIGNIGNIGNIG";
+    }
+  }
+  getPhoto() {
+    let points = this.state.puntuacion;
+    if (points <= 7) {
+      return "./img/tipo1.png";
+    } else if (points <= 21) {
+      return "./img/tipo2.png";
+    } else if (points <= 42) {
+      return "./img/tipo3.png";
+    } else if (points <= 68) {
+      return "./img/tipo4.png";
+    } else if (points <= 84) {
+      return "./img/tipo5.png";
+    } else {
+      return "./img/tipo6.png";
+    }
+  }
+
+  handleClick() {
+    this.setState({ lookAll: !this.state.lookAll });
+  }
+  getColor(el) {
+    if (this.state.answers.includes(el)) {
+      return "success";
+    } else {
+      return "secondary";
+    }
+  }
+  render() {
+    return (
+      <div>
+        <List className="list-questions">
+          {
+            this.state.listQuestions.map((e, i) => (
+              <>
+                <li>{e.question.text}</li>
+                <List>
+                  {
+                    e.question.answers.map(el => (
+                      <li>
+                        <Button color={this.getColor(el)} onClick={() => this.addAnswer(i, el)}>
+                          {el.points}-{el.answer}
+                        </Button>
+                      </li>
+                    ))
+                  }
+                </List>
+              </>
+            ))
+          }
+        </List>
+        {this.showRes()}
+
+        <Button onClick={() => this.handleClick()}>Mirar todos resultados</Button>
+        {/* {this.view()} */}
+        <ShowAnswers open={this.state.lookAll}></ShowAnswers>
+      </div>
+    );
+  }
 }
 
 export default App;
