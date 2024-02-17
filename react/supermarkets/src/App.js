0a13f64ef2b589b259bec107e98419c48951c51f
arya065 <arya065@g.educaand.es>
2024-02-17 21:27:11 +0100

    add popover hover

--- react/supermarkets/src/App.js
+++ react/supermarkets/src/App.js
@@ -2,24 +2,31 @@ import React, { Component, useState, useEffect } from "react";
 import { List, Button, Progress, Popover, PopoverHeader, PopoverBody } from 'reactstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-import axios from 'axios';
 // https://reactstrap.github.io/?path=/docs/components-popover--update
 
 function Cell(props) {
   const [status, setStatus] = useState(false);
+  const [shop, setShop] = useState(false);
   const handleClick = () => {
-    this.setState({ popOpen: !this.state.popover });
+    setStatus(!status);
+  }
+  const addShopIndex = (i) => {
+    
   }
   return (
-    <>
-      <Button id={"btn" + props.i} type="button" style={{ width: "60px", margin: "3px", background: this.mixColorsRGB([0, 255, 0], [200, 80, 255]) }} onClick={() => this.handleClick()}>
+    <span onMouseEnter={() => setStatus(true)} onMouseLeave={() => setStatus(false)}>
+      <Button id={"btn" + props.i} type="button" style={{ width: "60px", margin: "3px", background: props.mix([255, 100, 255], [255, 200, 255]) }} onClick={() => handleClick()}>
         {props.e}
       </Button>
-      <Popover isOpen={this.state.popOpen} flip target={"btn" + props.i} trigger="click">
+      <Popover isOpen={status} target={"btn" + props.i} trigger="legacy">
         <PopoverHeader>Quieres poner mercado aqui?</PopoverHeader>
-        <PopoverBody>si</PopoverBody>
+        <PopoverBody>
+          <div>Poblacion:{props.e}</div>
+          <div>Index de zona:{props.e}</div>
+          <Button>Si</Button>
+        </PopoverBody>
       </Popover>
-    </>
+    </span>
   )
 }
 class App extends Component {
@@ -36,22 +43,17 @@ class App extends Component {
         0, 2, 36, 43, 61, 26, 64, 12, 0,
         1, 2, 15, 43, 34, 2, 12, 2, 3,
         1, 0, 12, 3, 0, 0, 21, 2, 2
-      ],
-      popOpen: false,
+      ]
     };
   }
   mixColorsRGB(color1, color2) {
-    // let red = 0, green = 0, blue = 0;
     let red = color1[0] + color2[0];
     let green = color1[1] + color2[1];
     let blue = color1[2] + color2[2];
     return ("rgb(" + Math.round(red / 2) + "," + Math.round(green / 2) + "," + Math.round(blue / 2) + ")");
   }
-  handleClick() {
-    this.setState({ popOpen: !this.state.popover });
-  }
+
   render() {
-    // console.log(this.mixColorsRGB([255, 255, 255], [58, 89, 89]));
     return (
       <div>
         {this.state.poblacion.map((e, i) => {
@@ -59,18 +61,11 @@ class App extends Component {
             return (<br />)
           }
           return (
-            <></>
+            <>
+              <Cell mix={(color1, color2) => this.mixColorsRGB(color1, color2)} e={e} i={i}></Cell>
+            </>
           )
         })}
-        <>
-          <Button id={"h"} type="button" style={{ width: "60px", margin: "3px", background: this.mixColorsRGB([0, 255, 0], [200, 80, 255]) }} onClick={() => this.handleClick()}>
-            test
-          </Button>
-          <Popover isOpen={this.state.popOpen} flip target={"h"} trigger="click">
-            <PopoverHeader>Quieres poner mercado aqui?</PopoverHeader>
-            <PopoverBody>si</PopoverBody>
-          </Popover>
-        </>
       </div>
     )
   }
